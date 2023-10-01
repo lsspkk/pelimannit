9f4de9c343328ddcd1e486f51ba85cf372fb8876
Lasse Pekkarinen <lvp@iki.fi>
2023-10-01 22:46:51 +0300

    Update NpBackButton.tsx

--- nuottikokoelmat/components/NpBackButton.tsx
+++ nuottikokoelmat/components/NpBackButton.tsx
@@ -5,7 +5,6 @@ import { NpButton } from '@/components/NpButton'
 export const NpBackButton = ({ onClick, text, className }: { onClick: () => void; text?: string, className?: string } 
 ) => (
   <NpButton
-    {...props}
     className={`fixed top-0 right-0 border border-gray-300 shadow-md px-[7px] py-[2px] m-[5px]  rounded-full z-0 ${className}`}
     onClick={onClick}
   >
