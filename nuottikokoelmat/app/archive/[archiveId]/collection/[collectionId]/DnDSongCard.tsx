8a2cf2edf973913f0d61c3fa5195df5d77bc7db5
lsspkk <lsspkk@gmail.com>
2024-05-17 15:49:50 +0300

    Fixed DND song view

--- nuottikokoelmat/app/archive/[archiveId]/collection/[collectionId]/DnDSongCard.tsx
+++ nuottikokoelmat/app/archive/[archiveId]/collection/[collectionId]/DnDSongCard.tsx
@@ -1,38 +1,34 @@
 'use client'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+import { DragIcon } from '@/components/icons/DragIcon'
+import { NpButtonCard } from '@/components/NpButtonCard'
 import { Song } from '@/models/song'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import { NpButtonCard } from '@/components/NpButtonCard'
-import { DragIcon } from '@/components/icons/DragIcon'
+import { useRouter } from 'next/navigation'
+import React from 'react'
 import { displayPath } from './displayPath'
 
-export const DnDSongCard = ({ song, index, id }: { song: Song; index: number; id: string }) => {
-  const { listeners, setNodeRef, transform, transition } = useSortable({ id })
-  const router = useRouter()
+export const DnDSongCard = (
+	{ song, index, id, onLoadPdf }: { song: Song; index: number; id: string; onLoadPdf: (index: number) => void },
+) => {
+	const { listeners, setNodeRef, transform, transition } = useSortable({ id })
+	const router = useRouter()
 
-  const style = transform
-    ? {
-        transform: CSS.Transform.toString(transform),
-        transition,
-        background: 'rgba(235, 235, 235, 0.9)',
-      }
-    : undefined
-  return (
-    <div ref={setNodeRef} className='w-full' style={style}>
-      <NpButtonCard>
-        <div className='flex-col w-10/12 flex' onClick={() => router.push(song.url)}>
-          <div className='text-md'>{song.songname}</div>
-          <div className='text-xs overflow-clip text-clip whitespace-nowrap'>{displayPath(song)}</div>
-        </div>
-        <div className='flex-col w-2/12 items-end flex justify-center' {...listeners}>
-          <div className='flex flex-col items-center justify-center'>
-            <div className='text-xs '>{index + 1}</div>
-            <DragIcon />
-          </div>
-        </div>
-      </NpButtonCard>
-    </div>
-  )
+	const style = transform ? { transform: CSS.Transform.toString(transform), transition, background: 'rgba(235, 235, 235, 0.9)' } : undefined
+	return (
+		<div ref={setNodeRef} className='w-full' style={style}>
+			<NpButtonCard>
+				<div className='flex-col w-10/12 flex' onClick={() => onLoadPdf(index)}>
+					<div className='text-md'>{song.songname}</div>
+					<div className='text-xs overflow-clip text-clip whitespace-nowrap'>{displayPath(song)}</div>
+				</div>
+				<div className='flex-col w-2/12 items-end flex justify-center' {...listeners}>
+					<div className='flex flex-col items-center justify-center'>
+						<div className='text-xs '>{index + 1}</div>
+						<DragIcon />
+					</div>
+				</div>
+			</NpButtonCard>
+		</div>
+	)
 }
