c33bde480753b199c14f7386df477040139d3653
lsspkk <lsspkk@gmail.com>
2025-05-01 19:05:03 +0300

    fix session

--- nuottikokoelmat/models/session.ts
+++ nuottikokoelmat/models/session.ts
@@ -5,16 +5,16 @@ import { ArchiveVisitor } from './archiveVisitor'
 import { User } from './user'
 
 export const sessionOptions: SessionOptions = {
-	password: process.env.SECRET_COOKIE_PASSWORD as string,
-	cookieName: 'iron-session/examples/next.js',
-	// secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-	cookieOptions: { secure: process.env.NODE_ENV === 'production' },
+  password: process.env.SECRET_COOKIE_PASSWORD as string,
+  cookieName: 'nuottikokoelmat',
+  // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
+  cookieOptions: { secure: process.env.NODE_ENV === 'production' },
 }
 
 declare module 'iron-session' {
-	interface IronSessionData {
-		user?: User
-		archiveUser?: ArchiveUser
-		archiveVisitor?: ArchiveVisitor
-	}
+  interface IronSessionData {
+    user?: User
+    archiveUser?: ArchiveUser
+    archiveVisitor?: ArchiveVisitor
+  }
 }
